f7c6baed6debeb377ae7cc7552c9a9b93e2c3d4b
Kurtis-Or <124301427+Kurtis-Or@users.noreply.github.com>
2025-02-19 11:19:45 -0800

    All of the files used in project except set-up

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dlkor/raw"
+processed_folder_path = "/mnt/formula1dlkor/processed"
+presentation_folder_path = "/mnt/formula1dlkor/presentation"
